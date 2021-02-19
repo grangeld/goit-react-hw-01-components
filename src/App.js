ca65988fb99f2075ddd69f816a9169713f9776de
demon <your_email@whatever.com>
2021-02-19 18:18:05 +0200

    add Statistics component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import Profile from './component/Profile';
+import statisticalData from './statistical-data.json';
+import Statistics from './component/Statistics';
 import user from './user.json';
 
 function App() {
   return (
-    <Profile
-      name={user.name}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData} />
+    </>
   );
 }
 
---
+++ src/component/Statistics/Statistics.js
@@ -0,0 +1,35 @@
+import s from './Statistics.module.css';
+
+function Statistics({ title, stats }) {
+  return (
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title.toUpperCase()}</h2>}
+      <ul className={s['stat-list']}>
+        {stats.map(item => (
+          <li
+            key={item.id}
+            style={{ backgroundColor: randomColor() }}
+            className={s.item}
+          >
+            <span className={s.label}>{item.label}</span>
+            <span className={s.percentage}>{item.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function randomColor() {
+  const brightness = 5;
+  var rgb = [Math.random() * 256, Math.random() * 256, Math.random() * 256];
+  var mix = [brightness * 51, brightness * 51, brightness * 51]; //51 => 255/5
+  var mixedrgb = [rgb[0] + mix[0], rgb[1] + mix[1], rgb[2] + mix[2]].map(
+    function (x) {
+      return Math.round(x / 2.0);
+    },
+  );
+  return 'rgb(' + mixedrgb.join(',') + ')';
+}
+
+export default Statistics;
